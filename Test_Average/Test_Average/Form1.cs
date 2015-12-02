b1271b173207e9813a4b61604b47b13597cec98a
IanIsaac33 <iaisaac@geneva.edu>
2015-12-01 19:51:43 -0500

    test

---
+++ Test_Average/Test_Average/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double test1; // hold testscore 1
+                double test2; // hold testscore 2
+                double test3; // hold testscore 3
+                double average; // hold the average
+
+                // Get the scores
+                test1 = double.Parse(test1TextBox.Text);
+                test2 = double.Parse(test2TextBox.Text);
+                test3 = double.Parse(test3TextBox.Text);
+
+                // calculate the average
+                average = (test1 + test2 + test3) / 3.0;
+
+                // Display the average
+                averageLabel.Text = average.ToString("n1");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            test1TextBox.Text = " ";
+            test2TextBox.Text = " ";
+            test3TextBox.Text = " ";
+            averageLabel.Text = " ";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
